611fee739d00897632ca16b0d1fb803078b0829e
Ben Beattie <157050521+bbeattie-1@users.noreply.github.com>
2025-05-01 01:24:29 +0000

    Updated Template

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <script setup>
 import "@esri/calcite-components/dist/components/calcite-shell";
 
-import CardPanel from "@/components/Card Panel/CardPanel.vue";
-import MapPanel from "@/components/Map Panel/MapPanel.vue";
-import Navbar from "@/components/Navigation/Navbar.vue";
-import EditPanel from "@/components/Edit Panel/EditPanel.vue";
-
+import Map from "./components/Map.vue";
 
 </script>
 
 <template>
   <calcite-shell>
-    <Navbar/>
-<!--    <EditPanel/>-->
-    <MapPanel/>
-    <CardPanel/>
+    <Map />
   </calcite-shell>
 
 </template>
---
+++ src/components/Map.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import "@arcgis/map-components/dist/components/arcgis-map";
+
+</script>
+
+<template>
+
+    <arcgis-map zoom="4" center="-92,40"></arcgis-map>
+
+</template>
+
+<style scoped>
+
+arcgis-map {
+    width: 100%;
+    height: 100%;
+}
+
+</style>
